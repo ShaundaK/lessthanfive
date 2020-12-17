84e3c5ac4b8ea47fa614a16a6fea8c3a5438930c
ShaundaK <sdking1913@gmail.com>
2020-12-16 23:52:34 -0600

    prints out smaller numbers from list from user input

--- lessthanfive.py
+++ lessthanfive.py
@@ -4,12 +4,18 @@
 #then make a new list that has all of the elements less than 5 and print it out
 #write in one line of python..
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#then ask user for a number and return a list that contains only elements from the original list that are smaller than the number given by the user
 
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 count = 0
+user = input("Enter a number.  ")
+user_input = int(user)
+
+#def smaller_numbers(number):
 for number in a: #"in" iterates through the word "banana"
-    if number < 5:
+    if number < user_input:
+        count = count + 1
         print(number)
 
-b = [1, 1, 2, 3]
-print(*b)
+#b = [1, 1, 2, 3]
+#print(*b)
