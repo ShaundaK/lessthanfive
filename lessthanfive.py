c69f2795726d1e968a527a987e208ef03c3ed39b
ShaundaK <sdking1913@gmail.com>
2020-12-16 23:14:55 -0600

    print on one line

--- lessthanfive.py
+++ lessthanfive.py
@@ -1,8 +1,15 @@
 #take a list and print out the elements of the list less than 5
 #so print out 1, 1, 2, 3
+
+#then make a new list that has all of the elements less than 5 and print it out
+#write in one line of python..
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 count = 0
 for number in a: #"in" iterates through the word "banana"
     if number < 5:
         print(number)
+
+b = [1, 1, 2, 3]
+print(*b)
